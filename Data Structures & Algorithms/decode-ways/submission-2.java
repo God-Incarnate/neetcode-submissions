ee42fdf58f8cc50b4e87e122af05d6768ed46ffc
Prashant Verma <prashant11111999@gmail.com>
2026-06-05 16:53:57 +0530

    Add: decode-ways - submission-2

---
+++ Data Structures & Algorithms/decode-ways/submission-2.java
@@ -0,0 +1,35 @@
+class Solution {
+    //Space optimized DP
+
+    public int numDecodings(String s) {
+
+        int n = s.length();
+
+        int next = 1;      // dp[i+1]
+        int nextNext = 0;  // dp[i+2]
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            int curr = 0;
+
+            if (s.charAt(i) != '0') {
+
+                curr = next;
+
+                if (i + 1 < n) {
+                    int num = (s.charAt(i) - '0') * 10
+                            + (s.charAt(i + 1) - '0');
+
+                    if (num >= 10 && num <= 26) {
+                        curr += (i + 2 == n) ? 1 : nextNext;
+                    }
+                }
+            }
+
+            nextNext = next;
+            next = curr;
+        }
+
+        return next;
+    }
+}
