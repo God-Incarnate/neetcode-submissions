1f0c30f5ddc62e30f4724f20b10063640fbfcf83
Prashant Verma <prashant11111999@gmail.com>
2026-06-05 18:26:14 +0530

    Add: pow-x-n - submission-0

---
+++ Data Structures & Algorithms/pow-x-n/submission-0.java
@@ -0,0 +1,27 @@
+class Solution {
+    public double myPow(double x, int n) {
+
+        long power = n;
+
+        // handle negative powers
+        if (power < 0) {
+            x = 1 / x;
+            power = -power;
+        }
+
+        double result = 1;
+
+        while (power > 0) {
+
+            // if odd power
+            if ((power & 1) == 1) {
+                result *= x;
+            }
+
+            x *= x;
+            power >>= 1;
+        }
+
+        return result;
+    }
+}
