0a63e432f56adf080fa493f2303fc34fe373a990
Prashant Verma <prashant11111999@gmail.com>
2026-06-26 17:58:07 +0530

    Add: find-duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-2.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        for( int i=0;i<nums.length;i++){
+            int pos=Math.abs(nums[i]);
+            if(nums[pos]<0) return pos;
+            nums[pos]*=-1;
+        }
+        return -1;
+    }
+}
